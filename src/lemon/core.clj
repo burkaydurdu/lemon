cb9bea2e755f6447f861d345cd382e033abc62fb
burkaydurdu <burkaydurdu@outlook.com>
2021-03-28 18:13:03 +0300

    Add when-n unless and mergep macros

--- src/lemon/core.clj
+++ src/lemon/core.clj
@@ -11,3 +11,26 @@
      (let [t (take 3 e)
            k (list (second t) (first t) (last t))]
        (recur (drop 2 e) (conj r k))))))
+
+(defmacro when-n
+  "when-n have three parameters.
+   default is default value
+   test is condition
+   body is return block"
+  [default test & body]
+  (list 'if test (cons 'do body) default))
+
+(defmacro unless
+  "unless"
+  [test & branches]
+  (conj (take 2 (reverse branches)) test 'if))
+
+(defmacro mergep
+  "expr is map
+   clauses has conditions and body
+   Example: (mergep {:a :b} true {:a :c})"
+  [expr & clauses]
+  (assert (even? (count clauses)))
+  (reduce (fn [k v]
+            (if (first v) (merge k (last v)) k))
+          (conj (partition 2 clauses) expr)))
