6f54ce08daab78fb43897bce6335323b913c154e
burkaydurdu <burkaydurdu@outlook.com>
2021-03-28 20:49:23 +0300

    Add clojurescript lib

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lemon "0.1.1"
+(defproject lemon "0.1.2"
   :description "Lemon library"
   :url "https://github.com/burkaydurdu/lemon"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -7,5 +7,6 @@
   :deploy-repositories [["releases" {:url "https://burkaydurdu.com/archiva/internal/releases"
                                      :signing {:gpg-key "90DA6DB7C14EE8A5"}}]
                         ["snapshots" "https://burkaydurdu.com/archiva/internal/snapshots"]]
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.520"]]
   :repl-options {:init-ns lemon.core})
