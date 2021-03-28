ab85c3e7c05621c95a5a66e5bdb9187437a9e5ba
burkaydurdu <burkaydurdu@outlook.com>
2021-03-28 18:12:32 +0300

    Add test for numord macro

--- test/lemon/core_test.clj
+++ test/lemon/core_test.clj
@@ -1,7 +1,21 @@
 (ns lemon.core-test
-  (:require [clojure.test :refer :all]
-            [lemon.core :refer :all]))
+  (:require [clojure.test :refer [deftest testing is]]
+            [lemon.core :as core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest numord-test
+  (testing "form-1"
+    (is (= (core/numord 2 < 3 < 4) true)))
+  (testing "form-2"
+    (is (= (core/numord -1 < 3 < 4) true)))
+  (testing "form-3"
+    (is (= (core/numord 6 < 3 < 4) false)))
+  (testing "form-4"
+    (is (= (core/numord 6 <= 6 < 7) true)))
+  (testing "form-5"
+    (is (= (core/numord 6 = 6 = 6) true)))
+  (testing "form-6"
+    (is (= (core/numord -6 <= 0 > -1) true)))
+  (testing "form-7"
+    (is (= (core/numord 1 > 6 > 7) false)))
+  (testing "form-8"
+    (is (= (core/numord -6 <= 6 > 5) true))))
